0b58c2d39654871376b22d7046129af400515929
Richard Perdaan <richardperdaan@live.nl>
2018-06-11 08:59:29 +0200

    upload manual

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Magento 2
-Voor de handleiding en meer informatie ga naar: 
-https://myparcelbe.github.io/magento/
+# Magento handleiding
+Voor de handleiding ga naar https://myparcelbe.github.io/magento
--- view/adminhtml/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    "map": {
-        "*": {
-            "myparcelbe_massaction": "MyParcelBE_Magento/js/mass-action",
-            "myparcelbe_newshipment": "MyParcelBE_Magento/js/new-shipment",
-            "myparcelbe_supporttab": "MyParcelBE_Magento/js/config/support_tab/projects"
-        }
-    }
-};
