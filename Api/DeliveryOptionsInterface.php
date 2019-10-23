20b22418f98f43a02b9a2f25e4a96b5e034439d7
Edie Lemoine <edie@myparcel.nl>
2019-10-23 12:29:24 +0200

    add endpoint to convert delivery options to shipping methods

--- Api/DeliveryOptionsInterface.php
+++ Api/DeliveryOptionsInterface.php
@@ -1,30 +1,36 @@
 <?php
 /**
  * The delivery_options interface
- *
  * If you want to add improvements, please create a fork in our GitHub:
  * https://github.com/myparcelbe
  *
  * @author      Reindert Vetter <info@sendmyparcel.be>
- * @copyright   2010-2019 MyParcel
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US  CC BY-NC-ND 3.0 NL
  * @link        https://github.com/myparcelbe/magento
+ * @copyright   2010-2019 MyParcel
  * @since       File available since Release v2.0.0
  */
 
 namespace MyParcelBE\Magento\Api;
 
-
 /**
  * Get delivery options
  */
-Interface DeliveryOptionsInterface
+interface DeliveryOptionsInterface
 {
     /**
      * Return all delivery options
      *
-     * @api
      * @return mixed[] All options
+     * @api
      */
     public function get();
+
+    /**
+     * @param mixed $deliveryOptions
+     *
+     * @return array
+     * @api
+     */
+    public function convertToShippingMethod($deliveryOptions): array;
 }
--- Model/Checkout/DeliveryOptions.php
+++ Model/Checkout/DeliveryOptions.php
@@ -15,8 +15,10 @@
 namespace MyParcelBE\Magento\Model\Checkout;
 
 
+use Exception;
 use MyParcelBE\Magento\Api\DeliveryOptionsInterface;
 use MyParcelBE\Magento\Model\Quote\Checkout;
+use MyParcelNL\Sdk\src\Factory\DeliveryOptionsAdapterFactory;
 
 class DeliveryOptions implements DeliveryOptionsInterface
 {
@@ -38,5 +40,36 @@ class DeliveryOptions implements DeliveryOptionsInterface
     public function get()
     {
         return $this->settings->getDeliveryOptions();
+    }
+
+    /**
+     * @param mixed $deliveryOptions
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function convertToShippingMethod($deliveryOptions): array
+    {
+        $response = [];
+
+        try {
+            foreach ($deliveryOptions as $value) {
+                $newDeliveryOptions = DeliveryOptionsAdapterFactory::create($value);
+
+                $response[] = $newDeliveryOptions->toArray();
+            }
+
+            $response = [
+                "code"    => "200",
+                "data"    => json_encode($response),
+            ];
+        } catch (Exception $e) {
+            $response = [
+                "code"    => "422",
+                "message" => $e->getMessage(),
+            ];
+        }
+
+        return $response;
     }
 }
